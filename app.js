8628ae896e60d29ad966f04ba25b954e507f5360
Anas Ali <anasalimgmt@gmail.com>
2024-04-14 12:56:21 +0500

    update

---
+++ app.js
@@ -0,0 +1,39 @@
+//         CHAPTER 21 TO 25
+
+//           QUESTION NO 1
+
+// var firstName = prompt("enter your first name")
+// var lastName = prompt("enter your last name")
+// var fullName = firstName + " " + lastName
+// document.write(`Hello ${fullName}`)
+
+//           QUESTION NO 2
+
+// var phone = prompt("Enter Phone Your Favourite Phone Name")
+// document.write(`My Favourite Phone is: ${phone} <br> Lenght of string: ${phone.length}`)
+
+//           QUESTION NO 3
+
+// var userInp = "Pakistani";
+// document.write(`String: ${userInp} <br>Index of 'n': ${userInp.indexOf("n")}`)
+
+//           QUESTION NO 4
+
+// var words = "Hello World";
+// document.write(`String: ${words} <br>Last Index of 'l': ${words.lastIndexOf("l")}`)
+
+//           QUESTION NO 5
+
+// var words = "Pakistani";
+// document.write(`String: ${words} <br>Character at index '3': ${words.charAt(3)} `)
+
+//           QUESTION NO 6
+
+// var firstName = prompt("Enter First Name");
+// var lastName = prompt("Enter Last Name");
+// var fullName = firstName.concat(" " + lastName); // .concat concreate krne ke kaam aayegaa
+// document.write(fullName)
+
+//           QUESTION NO 7
+
+
